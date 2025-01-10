7f16bc334060f59e2b016165d589f9874ab87f82
lorenzmarqueses <lorenz.marqueses22@gmail.com>
2025-01-10 11:47:32 +0800

    add dropdown menu component and integrate it into AppNavigation; implement logout functionality

--- src/components/app-navigation.tsx
+++ src/components/app-navigation.tsx
@@ -1,19 +1,49 @@
 "use client";
+
+import * as React from "react";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
 import {
   NavigationMenu,
-  NavigationMenuList,
-  NavigationMenuItem,
-  NavigationMenuTrigger,
   NavigationMenuContent,
+  NavigationMenuItem,
   NavigationMenuLink,
-} from "@radix-ui/react-navigation-menu";
-import Link from "next/link";
-import React from "react";
-import { navigationMenuTriggerStyle } from "./ui/navigation-menu";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { usePathname } from "next/navigation";
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { usePathname, useRouter } from "next/navigation";
 import Image from "next/image";
-import { cn } from "@/lib/utils";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  User,
+  CreditCard,
+  Settings,
+  Keyboard,
+  Users,
+  UserPlus,
+  Mail,
+  MessageSquare,
+  PlusCircle,
+  Plus,
+  Github,
+  LifeBuoy,
+  Cloud,
+  LogOut,
+} from "lucide-react";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 const items = [
   {
@@ -32,24 +62,28 @@ const items = [
 
 const AppNavigation: React.FC = () => {
   const pathname = usePathname();
+  const router = useRouter();
+
+  const logout = () => {
+    document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    router.push("/login");
+  };
 
   return (
-    <NavigationMenu className="bg-[#3E1F47]">
-      <NavigationMenuList className="flex justify-between py-4 max-w-[1200px] mx-auto">
-        <div className="flex items-center space-x-4">
-          <div className="flex items-center space-x-4 mr-32">
-            <Image
-              className="dark:invert"
-              src="/assets/images/logo.svg"
-              alt="Next.js logo"
-              width={35}
-              height={35}
-              priority
-            />
-            <NavigationMenuItem>
-              <span className="text-white">Survival Nexus</span>
-            </NavigationMenuItem>
-          </div>
+    <NavigationMenu className="bg-[#3E1F47] px-4 w-full max-w-none">
+      <NavigationMenuList className="flex justify-between w-[1200px]">
+        <NavigationMenuItem className="flex items-center space-x-4 py-4">
+          <Image
+            className="dark:invert"
+            src="/assets/images/logo.svg"
+            alt="Next.js logo"
+            width={35}
+            height={35}
+            priority
+          />
+          <span className="text-white">Survival Nexus</span>
+        </NavigationMenuItem>
+        <div className="flex flex-row space-x-2">
           {items.map((item) => (
             <NavigationMenuItem
               key={item.name}
@@ -62,10 +96,33 @@ const AppNavigation: React.FC = () => {
           ))}
         </div>
         <NavigationMenuItem>
-          <Avatar className="border-2 border-white">
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Avatar className="border-2 border-white cursor-pointer">
+                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-56">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  <User />
+                  <span>Profile</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Settings />
+                  <span>Settings</span>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={logout}>
+                <LogOut />
+                <span>Log out</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
